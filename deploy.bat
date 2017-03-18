4335c9c89d7fc22b4d336ce424d71978604b8096
Vincent Chan <chan.vin@gmail.com>
2017-03-17 21:12:34 -0800

    test commit

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+echo "Generating site..."
+hugo --theme=paperback
+set /p commitMsg= Enter a commit message for the SOURCE: 
+echo "Committing source..."
+git add --all
+git commit -m "%commitMsg%"
+cd public
+set /p commitMsg= Enter a commit message for the OUTPUT:
+git add --all
+git commit -m "%commitMsg%"
+git push -f origin master
+echo "Done!"
